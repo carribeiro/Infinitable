c72085c8f87a551de2e2181a855f103f3b93c101
Carlos Ribeiro <carribeiro@gmail.com>
2012-05-30 00:21:41 -0300

    Acertando .gitignore, de novo, e finalmente colocando os modelos

--- infinitable/tabletop/models.py
+++ infinitable/tabletop/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+# coding: utf-8
+from django.db.models.base import Model
+from django.db.models.fields import TextField, IntegerField
+from django.db.models.fields.related import ForeignKey
 
-# Create your models here.
+class Panel(Model):
+    tag = TextField(max_length=30)
+    parent = ForeignKey('Panel')
+
+class Display(Panel):
+    slots = IntegerField()
